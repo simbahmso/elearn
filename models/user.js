e1fc6ad442054b0757a03337f2cbdee52c529d4c
DESKTOP-OART1FI\simba <simbahmso@yahoo.com>
2016-12-06 10:10:20 -0800

    Initial Commit

---
+++ models/user.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose'); 
+
+// User Schema
+var classSchema = mongoose.Schema({
+	username: {
+		type: String
+	},
+	email: {
+		type: String
+	},
+	password: {
+		type: String
+	},
+	type: {
+		type: String
+	}
+})
+
+var Class = module.exports = mongoose.model('User', UserSchema);
+
+// Fetch All Classes
+module.exports.getUserById = function(id ,callback){
+	User.findById(id, callback);
+}
+
+// Fetch  Single Class
+module.exports.getClassById = function(username, callback){
+	var query = {username: username};
+	Class.findOne(query, callback);
+}
